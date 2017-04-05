c963fd990043420c50839027b16aee7d4c3396e2
mk4mineko <marcello.annicchiarico@mineko.de>
2017-04-05 18:11:34 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function() {
+  
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+         $('.main-menu').addClass('stiky');
+      } else {
+         $('.main-menu').removeClass('stiky');
+      }
+  });
+  
+  $('.showhide.more, .showhide.less').click(function() {
+   $(this).parent().parent().find('.serve').toggleClass('active');
+  });
+  
+  $('.btn-bur').click(function() {
+    $('.main-menu-link').removeClass('slideleft');
+    $('.main-menu-link').addClass('slideright');
+  });
+  $('.btn-clo').click(function() {
+    $('.main-menu-link').removeClass('slideright');
+    $('.main-menu-link').addClass('slideleft');
+  });
+  
+  $.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+  }
+  if($.urlParam('back')){
+    $('.main-head').css('background','url("assets/back/'+$.urlParam('back')+'.jpg") center / cover')
+  }
+  
+});
