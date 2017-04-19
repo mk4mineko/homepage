c304b47f6ab51f0c5d5e0c49511df8ace4d81ff9
mk4mineko <marcello.annicchiarico@mineko.de>
2017-04-19 12:18:53 +0200

    WIP

--- index.js
+++ index.js
@@ -21,17 +21,27 @@ $(function() {
     $('.main-menu-link').addClass('slideleft');
   });
   
-//  $.urlParam = function(name){
-//    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-//    return results[1] || 0;
-//  }
-//  if($.urlParam('back')){
-//    $('.main-head').css('background','url("assets/back/'+$.urlParam('back')+'.jpg") center / cover')
-//  }
   
   
-//  $(".post-entry p:contains('[mineko_call_to_action]')").css( "display", "none" );
-//  $(".post-entry p:contains('[mineko_button style=“solid“]')").css( "display", "none" );
-//  $(".post-entry p:contains('[/mineko_call_to_action]')").css( "display", "none" );
+  var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : sParameterName[1];
+          }
+      }
+  };
+  
+  var back = getUrlParameter('back');
+  
+  if(back){
+    $('.main-head').css('background','url("assets/back/'+back+'.jpg") center / cover')
+  }
   
 });
